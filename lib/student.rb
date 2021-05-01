8832e28102d037f267eb706c69d2876bcb1f3911
esellers25 <erin.e.sellers@gmail.com>
2021-05-01 12:38:42 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
+    attr_writer :knowledge
+    
+    def initialize
+        @knowledge = []
+    end 
 
+    def learn(lesson)
+        self.knowledge << lesson
+    end 
+
+    def knowledge
+        @knowledge
+    end 
 end
